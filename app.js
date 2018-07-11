19aa40f93c3313ff4fc36163dfb757f3274774f5
Ahmed Hammad <hammad97official@gmail.com>
2018-07-11 02:04:23 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -37,10 +37,14 @@ app.post('/login',urlencodedParser,(req,res)=>{
 
   con.query(`SELECT PASSWORD FROM data WHERE USERNAME = '${username}'`, (err,result)=>{
     if(err) throw err;
-    bcrypt.compare(password, result[0].PASSWORD, function(err, rep) {
-      if(rep){res.render('message',{message: "Logged in successfully."});}
-      else{res.render('message',{message: "Wrong data, try again."});}
-    });
+    if(result.length!=0){
+      bcrypt.compare(password, result[0].PASSWORD, function(err, rep) {
+        if(rep){res.render('message',{message: "Logged in successfully."});}
+        else{res.render('message',{message: "Wrong data, try again."});}
+      });
+    }else{
+      res.render('message',{message: "No such a user!"});
+    }
   });
 
 });
@@ -49,12 +53,12 @@ app.post('/signup',urlencodedParser,(req,res)=>{
   let username = req.body.username;
   let password = req.body.password;
 
-    con.query(`SELECT USERNAME FROM data WHERE USERNAME = '${username}'`, (err, result)=> {
-      if (err) throw err;
+    con.query(`SELECT USERNAME FROM data WHERE USERNAME = '${username}'`, (err1, result)=> {
+      if (err1) throw err1;
       if(result.length == 0){
-        bcrypt.hash(password, 10, function(err, hash) {
-          con.query(`INSERT INTO data SET ?` , {USERNAME: username, PASSWORD: hash}, (err,result)=>{
-            if(err) throw err;
+        bcrypt.hash(password, 10, function(err2, hash) {
+          con.query(`INSERT INTO data SET ?` , {USERNAME: username, PASSWORD: hash}, (err3,result)=>{
+            if(err3) throw err3;
             else{res.render('message',{message: "Signed up successfully"});}
           });
         });
